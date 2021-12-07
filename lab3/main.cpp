13d4fc218453672da32a7b3c410d277a696b5ba0
Dimanteo <dikolobay@gmail.com>
2021-12-07 22:56:53 +0300

    [lab3] Home work 3 done

--- lab3/main.cpp
+++ lab3/main.cpp
@@ -9,7 +9,9 @@ using namespace coma;
 void printRoot(const Vector &v) { std::cout << "x = " << v(0) << "\ny = " << v(1) << "\n"; };
 
 void task12_2() {
-    std::cout << "## Задача 12.2 ##\n";
+    std::cout << "#################\n";
+    std::cout << "#  Задача 12.2  #\n";
+    std::cout << "#################\n";
 
     NewtonSolver solver(2);
     FunctionMatrix &funcVec = *solver.function();
@@ -61,10 +63,13 @@ void task12_2() {
     printRoot(positive_root);
     std::cout << "Negative root:\n";
     printRoot(negative_root);
+    std::cout << "\n";
 }
 
 void task12_7() {
-    std::cout << "## Задача 12.7(г) ##\n";
+    std::cout << "##################\n";
+    std::cout << "# Задача 12.7(г) #\n";
+    std::cout << "##################\n";
 
     NewtonSolver solver(2);
     FunctionMatrix &funcVec = *solver.function();
@@ -100,10 +105,57 @@ void task12_7() {
     std::cout.precision(5);
     std::cout << "Root:\n";
     printRoot(root);
+    std::cout << "\n";
+}
+
+void task12_8() {
+    std::cout << "##################\n";
+    std::cout << "# Задача 12.8(c) #\n";
+    std::cout << "##################\n";
+
+    NewtonSolver solver(2);
+    FunctionMatrix &funcVec = *solver.function();
+    FunctionMatrix &jacobian = *solver.jacobian();
+
+    funcVec.func(0, 0) = [](FunctionMatrix::ArgTy point) {
+        numb_t y = point(1);
+        return y - 1.0 / 4.0 / M_E;
+    };
+    funcVec.func(1, 0) = [](FunctionMatrix::ArgTy point) {
+        numb_t x = point(0);
+        numb_t y = point(1);
+        return y - log(x) / (x * x);
+    };
+
+    jacobian.func(0, 0) = [](FunctionMatrix::ArgTy point) { return 0; };
+    jacobian.func(0, 1) = [](FunctionMatrix::ArgTy point) { return 1; };
+    jacobian.func(1, 0) = [](FunctionMatrix::ArgTy point) {
+        numb_t x = point(0);
+        return -(1 - 2.0 * log(x)) / pow(x, 3);
+    };
+    jacobian.func(1, 1) = [](FunctionMatrix::ArgTy point) { return 1; };
+
+    numb_t precision = 1e-4;
+    Vector left(2);
+    left << 1, 1 / 4 / M_E;
+    Vector right(2);
+    right << 4, 1 / 4 / M_E;
+
+    Vector left_root = solver.solve(left, precision);
+    Vector right_root = solver.solve(right, precision);
+    numb_t distance = right_root(0) - left_root(0);
+
+    std::cout.precision(4);
+    std::cout << "Smaller root:\n";
+    printRoot(left_root);
+    std::cout << "Greater root:\n";
+    printRoot(right_root);
+    std::cout << "Width: " << distance << "\n";
 }
 
 int main() {
     task12_2();
     task12_7();
+    task12_8();
     return 0;
 }
